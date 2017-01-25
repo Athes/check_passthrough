5ac3e72d5889cd10609762fcb2b3275501fc850e
Athes <tokaji.attila@gmx.com>
2017-01-25 14:08:06 +0100

    Update check_passthru.sh

--- check_passthru.sh
+++ check_passthru.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Usage: check_passthru.sh <admin_password> <list of module IPs, separated with spaces>
+# Usage: check_passthru.sh <admin_password> "<list of module IPs, separated with spaces, between quotes>"
 Fault=`for i in $2;do echo $i: |tr -d "\n"; sshpass -p $1 ssh admin@$i show chassis|grep Fault;done|grep On`
 if  [[ $? -eq "0" ]];then
 echo "Module error: " $Fault
